184f9aa28f3eaa344c189927c90f32697c4a688d
HarshAgarwal4 <agarwalh270@gmail.com>
2025-06-16 16:03:42 +0530

    H

--- server/controllers/staticController.js
+++ server/controllers/staticController.js
@@ -11,7 +11,7 @@ function HomePage(req,res){
 "I believe I should be hired for this internship because I have a strong foundation in web development with experience in HTML, CSS, JavaScript, React, MongoDB, Express, and Node.js. I'm passionate about building functional, user-friendly applications and am eager to apply my skills in a real-world environment. I’m a quick learner, adaptable, and excited to contribute to the team while continuing to grow and develop my abilities in web development."
 
 function signUpPage(req,res){
-    res.render('signUp')
+    res.render('signup')
 }
 
 function loginPage(req,res){
